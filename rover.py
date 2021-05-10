83015860a76129ca039bf7f4735f3660eb039e52
devittr <devittr@tcd.ie>
2021-05-10 19:33:24 +0100

    Add data structure for compass and keeping track of rover direction

--- rover.py
+++ rover.py
@@ -1,3 +1,5 @@
+import dataclasses
+
 from planet import Planet
 
 # Movement constants
@@ -7,16 +9,32 @@ LEFT = "L"
 RIGHT = "R"
 
 
+@dataclasses.dataclass(frozen=True)
+class Direction:
+    x: int
+    y: int
+    s: str
+
+
+EAST = Direction(1, 0, "E")
+NORTH = Direction(0, 1, "N")
+WEST = Direction(-1, 0, "W")
+SOUTH = Direction(0, -1, "S")
+COMPASS = [EAST, SOUTH, WEST, NORTH]
+NDIRS = len(COMPASS)
+
+
 class Rover:
 
-    def __init__(self, planet: Planet):
+    def __init__(self, planet: Planet, dir_index: int):
         self.planet = planet
+        self.__dir_index = dir_index
 
     def rotate_clockwise(self):
-        pass
+        self.__dir_index = (self.__dir_index + 1) % NDIRS
 
     def rotate_anticlockwise(self):
-        pass
+        self.__dir_index = (self.__dir_index - 1) % NDIRS
 
     def move_forward(self):
         pass
