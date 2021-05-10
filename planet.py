4d75426c4fe763e2b04a1594b4656c911b93d61d
devittr <devittr@tcd.ie>
2021-05-10 19:28:19 +0100

    Define initial interfaces for planet and rover

---
+++ planet.py
@@ -0,0 +1,8 @@
+class Planet:
+
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+
+    def position_is_valid(self, x: int, y: int) -> bool:
+        pass
---
+++ rover.py
@@ -0,0 +1,26 @@
+from planet import Planet
+
+# Movement constants
+FORWARD = "F"
+BACKWARD = "B"
+LEFT = "L"
+RIGHT = "R"
+
+
+class Rover:
+
+    def __init__(self, planet: Planet):
+        self.planet = planet
+
+    def rotate_clockwise(self):
+        pass
+
+    def rotate_anticlockwise(self):
+        pass
+
+    def move_forward(self):
+        pass
+
+    def move_backward(self):
+        pass
+
