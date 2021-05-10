f3fe4b1ffe61802780cfd79e60c8409bba8ce305
devittr <devittr@tcd.ie>
2021-05-10 20:21:11 +0100

    Add CLI for rover initalisation, instructions and basic display

--- planet.py
+++ planet.py
@@ -1,8 +1,12 @@
 class Planet:
 
     def __init__(self, width: int, height: int):
+        assert width > 0 and height > 0, "Height and with must be positive integers"
         self.width = width
         self.height = height
 
     def position_is_valid(self, x: int, y: int) -> bool:
         return 0 <= x <= self.width and 0 <= y <= self.height
+
+    def __str__(self):
+        return f"Planet width={self.width} height={self.height}"
--- rover.py
+++ rover.py
@@ -1,4 +1,5 @@
 import dataclasses
+import traceback
 from typing import Tuple
 
 from planet import Planet
@@ -15,6 +16,9 @@ class Direction:
     x: int
     y: int
     s: str
+
+    def __str__(self):
+        return self.s
 
 
 EAST = Direction(1, 0, "E")
@@ -54,11 +58,56 @@ class Rover:
         return self.__xpos, self.__ypos
 
     def _make_move(self, xmove: int, ymove: int):
-        new_xpos = self.__xpos + xmove
-        new_ypos = self.__ypos + ymove
+        new_xpos = (self.__xpos + xmove)
+        new_ypos = (self.__ypos + ymove)
         if self.planet.position_is_valid(new_xpos, new_ypos):
             self.__xpos, self.__ypos = new_xpos, new_ypos
         else:
             print(f"Error: cannot move to {new_xpos, new_ypos}")
 
+    def __str__(self):
+        return f"Rover at x={self.__xpos} y={self.__ypos}, facing {self.direction.s}\n{self.planet}"
+
+
+if __name__ == "__main__":
+
+    rover = None
+    while rover is None:
+        try:
+            width, height = map(int, input("Enter Planet width and height: ").split())
+            planet = Planet(width, height)
+
+            x, y, direction = input("Enter initial rover position e.g. '0 0 E': ").split()
+            x, y = int(x), int(y)
+            d = direction.upper()
+            if d == str(EAST):
+                dir_index = COMPASS.index(EAST)
+            elif d == str(NORTH):
+                dir_index = COMPASS.index(NORTH)
+            elif d == str(WEST):
+                dir_index = COMPASS.index(WEST)
+            elif d == str(SOUTH):
+                dir_index = COMPASS.index(SOUTH)
+            else:
+                raise Exception(f"Invalid direction {direction}")
+
+        except Exception:
+            print("Error parsing input")
+            print(traceback.format_exc())
+        else:
+            rover = Rover(planet, x, y, dir_index)
+
+    while True:
+        print(rover)
+        instruction = input("Enter next instruction: ").upper()
+        if instruction == FORWARD:
+            rover.move_forward()
+        elif instruction == BACKWARD:
+            rover.move_backward()
+        elif instruction == RIGHT:
+            rover.rotate_clockwise()
+        elif instruction == LEFT:
+            rover.rotate_anticlockwise()
+        else:
+            print(f"Could not interpret instruction: {instruction}")
 
